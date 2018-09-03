1b9e64aab74a3db1cab15233bf6293db0915ee3c
Rockydonthurtem <charrisq@hotmail.com>
2018-09-03 07:54:48 -0500

    mini

--- src/Card.js
+++ src/Card.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import './Card.css';
-
+import React from "react";
+import "./Card.css";
 
 export default function Card({ todo, toggle, removeTodo }) {
-    return (
-        <div className='card-container'>
-            <div 
-                onClick={ () => toggle(todo.key) }
-                className={ todo.data.completed ? 'check toggle-green' : 'check' }
-                ></div> 
-            <div 
-                className={ todo.data.completed ? 'todo completed' : 'todo'}
-                >{ todo.data.todo }</div> 
-            <div 
-                onClick={ () => removeTodo(todo.key) }
-                className='remove'>X</div> 
-        </div> 
-    )
+  return (
+    <div
+      className="card-container"
+      style={{ height: todo.style.height, opacity: todo.style.opacity }}
+    >
+      <div
+        onClick={() => toggle(todo.key)}
+        className={todo.data.completed ? "check toggle-green" : "check"}
+      />
+      <div className={todo.data.completed ? "todo completed" : "todo"}>
+        {todo.data.todo}
+      </div>
+      <div onClick={() => removeTodo(todo.key)} className="remove">
+        X
+      </div>
+    </div>
+  );
 }
